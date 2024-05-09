288c7744cb91e4857ea93aa067517118192608ff
TimePrecursor <21006@fraser.school.nz>
2024-05-10 10:18:27 +1200

    Created the first code (the window for the GUI)

--- FINAL_CeasarCypher.py
+++ FINAL_CeasarCypher.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# lol
+
+# This will import all the widgets
+# and modules which are available in
+# tkinter and ttk module
+from tkinter import *
+from tkinter.ttk import *
+
+# creates a Tk() object
+root = Tk()
+
+
+
+
+
+
+
+mainloop()
