0bad2eb0210b3b4827a46b9f75a55509ce7077a1
TimePrecursor <21006@fraser.school.nz>
2024-05-10 10:35:36 +1200

    added main functions

--- FINAL_CeasarCypher.py
+++ FINAL_CeasarCypher.py
@@ -41,5 +41,85 @@ key = IntVar()
 Entry(root, textvariable=key, width=10,font=("Arial","20")).pack(pady=20)
 
 
+# create a label that shows last output
+labelez = Label(root, text =(" "))
+labelez.pack(pady = 10)
+
+
+# add a button widget which will decrypt or encrypt
+# while clearing previous text in the entry 
+btn = Button(root, text ="Click to Decrypt", 
+			command= lambda:(decrypt(),clear_text()))
+btn.pack(pady = 10)
+btn = Button(root, text ="Click to Encrypt", 
+			command= lambda:(encrypt(),clear_text()))
+btn.pack(pady = 10)
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#                        The Code
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+alphabet3 = StringVar()
+alphabet2 = []
+alphabet1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ']
+for x, y in enumerate(alphabet1):
+    y = y.lower()
+    alphabet2.insert(x,y)
+alphabet3 = ''.join(alphabet1)
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#                        The Functions
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def decrypt():
+    global newmsg
+    newmsg = []
+    key1 = key.get()
+    msg = message.get()
+    msg = msg.lower()
+    try:    
+        for count, item in enumerate(msg):
+            pos1 = alphabet2.index(item)
+            pos2 = pos1 - key1
+            newmsg.append((alphabet1[pos2]))
+    except:
+        updatelabel()        
+    newmsg = ''.join(newmsg)
+    newmsg2 = StringVar()
+    newmsg2.set(newmsg)
+    copytoclip(newmsg2.get())
+
+
+# create the encrypt function to encrypt
+# and copy the output to clipboard
+def encrypt():
+    global newmsg
+    newmsg = []
+    key1 = key.get()
+    msg = message.get()
+    msg = msg.lower()
+    try:    
+        for count, item in enumerate(msg):
+            pos1 = alphabet2.index(item)
+            pos2 = pos1 - key1
+            newmsg.append((alphabet1[pos2]))
+    except:
+        updatelabel()
+        
+    newmsg = ''.join(newmsg)
+    newmsg = ''.join(newmsg)
+    newmsg2 = StringVar()
+    newmsg2.set(newmsg)
+    copytoclip(newmsg2.get())
+    
+
 
 mainloop()
