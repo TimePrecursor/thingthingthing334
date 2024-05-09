188ae796ad69decc610c8eb687c147af01ac122e
TimePrecursor <21006@fraser.school.nz>
2024-05-10 09:58:15 +1200

    Added some descriptions

--- CeasarCypher2.py
+++ CeasarCypher2.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
-
+# This will import all the widgets
+# and modules which are available in
+# tkinter and ttk module
 from tkinter import *
 from tkinter.ttk import *
 
 # creates a Tk() object
 root = Tk()
+
+
+
+
+
+# mainloop, runs infinitely code is stopped
+mainloop()
